c99fb157f283aecf119c74481b96ab6b0886e3c6
Muhammadakmal14 <146051828+Muhammadakmal14@users.noreply.github.com>
2023-12-07 14:15:15 +0700

    Add files via upload

---
+++ hasil.py
@@ -0,0 +1,11 @@
+from bangun_datar import *
+
+persegi(5)
+persegi_panjang(5, 2)
+segitiga_sama_sisi(20, 25)
+jajar_genjang(6, 8, 10)
+lingkaran(20)
+belah_ketupat(6, 8, 10)
+layang_layang(4, 6, 8, 10)
+
+
